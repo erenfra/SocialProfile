158a44e913bf150a1c72a8b3b70e584d51692efe
Renato Oliveira Fraga <rofraga74@gmail.com>
2022-12-05 11:46:53 -0600

    Initial Commit

---
+++ SocialProfile/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SocialProfile
+//
+//  Created by Renato Oliveira Fraga on 12/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SocialProfile/SocialProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialProfileApp.swift
+//  SocialProfile
+//
+//  Created by Renato Oliveira Fraga on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SocialProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
