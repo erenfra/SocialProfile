cda7008312b40a8c59939c01d748511dff550833
Renato Oliveira Fraga <rofraga74@gmail.com>
2022-12-07 16:28:54 -0600

    App ready

--- SocialProfile/ContentView.swift
+++ SocialProfile/ContentView.swift
@@ -9,57 +9,106 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var billValue: String = ""
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 10
+    @State private var isZeroPercentPressed = true
+    @State private var isTenPercentPressed = false
+    @State private var isFifteenPercentPressed = false
+    @State private var isTwentyPercentPressed = false
+    @FocusState private var amountIsFocused: Bool
+    
+    
+    var perPerson: Double {
+        return checkAmount / Double(numberOfPeople)
+    }
+    
+    var totalPerPerson: Double {
+        if isZeroPercentPressed {
+            return perPerson
+        } else if isTenPercentPressed {
+            return perPerson * 1.1
+        } else if isFifteenPercentPressed {
+            return perPerson * 1.15
+        } else {
+            return perPerson * 1.2
+        }
+        
+    }
+    
     
     var body: some View {
         VStack {
             ZStack {
                 LinearGradient(colors: [Color("grad_top"), Color("grad_bottom")], startPoint: .top, endPoint: .bottom)
+                    
                 VStack {
-                    Spacer(minLength: 50)
+                    Spacer()
                     Image("table")
                         .resizable()
                         .scaledToFit()
                         .frame(height: 150)
                         .padding(.vertical)
                     HStack {
-                        Text("$")
-                            .font(.title)
-                        TextField("", text: $billValue)
-                            .frame(width: 280, height: 40)
-                            //.padding(10)
+                      
+                        TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                            .font(.title2)
+                            .keyboardType(.decimalPad)
+                            .focused($amountIsFocused)
+                            .frame(width: 220, height: 20)
+                            .padding(10)
                             .background(RoundedRectangle(cornerRadius: 50).foregroundColor(.white))
                             .shadow(radius: 1.0, x: 1.0, y: 1.0)
-                        //.padding()
+
                     }
-                    //Spacer()
+
                     HStack(spacing: 50) {
                         Image(systemName: "person.3")
-                            .font(.title3)
+                            .font(.headline)
                             .frame(width: 50, height: 50)
                             .background(Color(.white))
                             .cornerRadius(50)
                             .shadow(radius: 1.0, x: 1.0, y: 1.0)
                         HStack(spacing: 15) {
-                            Image(systemName: "plus")
-                                 .font(.title3.bold())
-                                 .frame(width: 30, height: 30)
-                                 .background(Color(.white))
-                                 .cornerRadius(50)
-                                 .shadow(radius: 1.0, x: 1.0, y: 1.0)
-                            Text("10")
+                            Button(action: {
+                                if numberOfPeople >= 99 {
+                                    numberOfPeople = 99
+                                } else {
+                                    numberOfPeople += 1
+                                }
+                            }, label: {
+                                Image(systemName: "plus")
+                                     .font(.title3.bold())
+                                     .foregroundColor(.black)
+                                     .frame(width: 30, height: 30)
+                                     .background(Color(.white))
+                                     .cornerRadius(50)
+                                     .shadow(radius: 1.0, x: 1.0, y: 1.0)
+                            })
+                            
+                            Text(String(numberOfPeople))
                                 .font(.title.bold())
                                 .frame(width: 50, height: 50)
                                 .background(Color(.white))
                                 .cornerRadius(13)
                                 .shadow(radius: 1.0, x: 1.0, y: 1.0)
                             
-                            Image(systemName: "minus")
-                                 .font(.title3.bold())
-                                 .frame(width: 30, height: 30)
-                                 .background(Color(.white))
-                                 .cornerRadius(50)
-                                 .shadow(radius: 1.0, x: 1.0, y: 1.0)
+                            Button(action: {
+                                if numberOfPeople == 1 {
+                                    numberOfPeople = 1
+                                } else {
+                                    numberOfPeople -= 1
+                                }
+                            }, label: {
+                                Image(systemName: "minus")
+                                     .font(.title3.bold())
+                                     .foregroundColor(.black)
+                                     .frame(width: 30, height: 30)
+                                     .background(Color(.white))
+                                     .cornerRadius(50)
+                                     .shadow(radius: 1.0, x: 1.0, y: 1.0)
+                            })
+                            
                         }
                         
                     }
@@ -68,18 +117,28 @@ struct ContentView: View {
                 }
                 
             }
-            .frame(height: 350)
+            .toolbar(content: {
+                ToolbarItem(placement: .keyboard, content: {
+                    Button(action: {
+                        amountIsFocused = false
+                    }, label: {
+                        Text("Done")
+                            
+                    })
+                })
+            })
+            .frame(height: 370)
+            .cornerRadius(50)
             
             Spacer()
             VStack(spacing: 10) {
                 Text("Per Person")
                     .font(.title2.bold())
                 HStack {
-                    Text("$")
-                        .font(.title)
-                    Text("")
-                        .font(.largeTitle)
-                        .frame(width: 280, height: 40)
+
+                    Text(perPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .font(.title2)
+                        .frame(width: 200, height: 40)
                         .background(RoundedRectangle(cornerRadius: 50).foregroundColor(.white))
                         .shadow(radius: 1.0, x: 1.0, y: 1.0)
                 }
@@ -90,30 +149,66 @@ struct ContentView: View {
                 Text("Choose a tip")
                     .font(.title2.bold())
                 HStack(spacing: 20) {
-                    Text("0%")
-                        .font(.title3.bold())
-                        .frame(width: 50, height: 50)
-                        .background(Color("selected"))
-                        .cornerRadius(10)
-                        .shadow(radius: 1.0, x: 1.0, y: 1.0)
-                    Text("10%")
-                        .font(.title3.bold())
-                        .frame(width: 50, height: 50)
-                        .background(Color(.white))
-                        .cornerRadius(10)
-                        .shadow(radius: 1.0, x: 1.0, y: 1.0)
-                    Text("15%")
-                        .font(.title3.bold())
-                        .frame(width: 50, height: 50)
-                        .background(Color(.white))
-                        .cornerRadius(10)
-                        .shadow(radius: 1.0, x: 1.0, y: 1.0)
-                    Text("20%")
-                        .font(.title3.bold())
-                        .frame(width: 50, height: 50)
-                        .background(Color(.white))
-                        .cornerRadius(10)
-                        .shadow(radius: 1.0, x: 1.0, y: 1.0)
+                    Button(action: {
+                        isZeroPercentPressed = true
+                        isTenPercentPressed = false
+                        isFifteenPercentPressed = false
+                        isTwentyPercentPressed = false
+                        
+                    }, label: {
+                        Text("0%")
+                            .font(.title3)
+                            .foregroundColor(.black)
+                            .frame(width: 50, height: 50)
+                            .background(Color(isZeroPercentPressed ? "selected" : "notSelected"))
+                            .cornerRadius(10)
+                            .shadow(radius: 1.0, x: 1.0, y: 1.0)
+                    })
+                    
+                    Button(action: {
+                        isZeroPercentPressed = false
+                        isTenPercentPressed = true
+                        isFifteenPercentPressed = false
+                        isTwentyPercentPressed = false
+                    }, label: {
+                        Text("10%")
+                            .font(.title3)
+                            .foregroundColor(.black)
+                            .frame(width: 50, height: 50)
+                            .background(Color(isTenPercentPressed ? "selected" : "notSelected"))
+                            .cornerRadius(10)
+                            .shadow(radius: 1.0, x: 1.0, y: 1.0)
+                    })
+                    
+                    Button(action: {
+                        isZeroPercentPressed = false
+                        isTenPercentPressed = false
+                        isFifteenPercentPressed = true
+                        isTwentyPercentPressed = false
+                    }, label: {
+                        Text("15%")
+                            .font(.title3)
+                            .foregroundColor(.black)
+                            .frame(width: 50, height: 50)
+                            .background(Color(isFifteenPercentPressed ? "selected" : "notSelected"))
+                            .cornerRadius(10)
+                            .shadow(radius: 1.0, x: 1.0, y: 1.0)
+                    })
+                    Button(action: {
+                        isZeroPercentPressed = false
+                        isTenPercentPressed = false
+                        isFifteenPercentPressed = false
+                        isTwentyPercentPressed = true
+                    }, label: {
+                        Text("20%")
+                            .font(.title3)
+                            .foregroundColor(.black)
+                            .frame(width: 50, height: 50)
+                            .background(Color(isTwentyPercentPressed ? "selected" : "notSelected"))
+                            .cornerRadius(10)
+                            .shadow(radius: 1.0, x: 1.0, y: 1.0)
+                    })
+                    
                 }
             }
             
@@ -122,11 +217,9 @@ struct ContentView: View {
                 Text("Total per Person")
                     .font(.title2.bold())
                 HStack {
-                    Text("$")
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                         .font(.title)
-                    Text("")
-                        .font(.largeTitle)
-                        .frame(width: 280, height: 40)
+                        .frame(width: 200, height: 40)
                         .background(RoundedRectangle(cornerRadius: 50).foregroundColor(.white))
                     .shadow(radius: 1.0, x: 1.0, y: 1.0)
                 }
